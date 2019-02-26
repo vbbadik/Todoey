ca8ad0ac35e78248142e12a8552e1a918b6903e2
Vitaly Badion <vitalybadion@badik-imac.local>
2019-02-26 09:39:12 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Vitaly Badion on 2/26/19.
+//  Copyright © 2019 Vitaly Badion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
